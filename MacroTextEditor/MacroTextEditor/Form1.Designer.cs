54c0a7d9363fc519e97da44a0d573f50e7cb4b42
Oleksandr93 <vivalasasha@gmail.com>
2016-02-07 11:56:40 +0200

    MacroTextEditor

---
+++ MacroTextEditor/MacroTextEditor/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MacroTextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.macrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addAccemblyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runMacrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.macrosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "File";
+            // 
+            // macrosToolStripMenuItem
+            // 
+            this.macrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addAccemblyToolStripMenuItem,
+            this.runMacrosToolStripMenuItem});
+            this.macrosToolStripMenuItem.Name = "macrosToolStripMenuItem";
+            this.macrosToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.macrosToolStripMenuItem.Text = "Macros";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // addAccemblyToolStripMenuItem
+            // 
+            this.addAccemblyToolStripMenuItem.Name = "addAccemblyToolStripMenuItem";
+            this.addAccemblyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addAccemblyToolStripMenuItem.Text = "Add Accembly";
+            this.addAccemblyToolStripMenuItem.Click += new System.EventHandler(this.addAccemblyToolStripMenuItem_Click);
+            // 
+            // runMacrosToolStripMenuItem
+            // 
+            this.runMacrosToolStripMenuItem.Name = "runMacrosToolStripMenuItem";
+            this.runMacrosToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.runMacrosToolStripMenuItem.Text = "Run Macros";
+            this.runMacrosToolStripMenuItem.Click += new System.EventHandler(this.runMacrosToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 243);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem macrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addAccemblyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runMacrosToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ MacroTextEditor/MacroTextEditor/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+
+namespace MacroTextEditor
+{
+    public partial class Form1 : Form
+    {
+
+        OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+        SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+
+        // Коллекция для загружаемых сборок
+        private List<string> accemblyList; 
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Устанавливаем типы файлов для диалоговых окон
+            OpenFileDialog1.Filter = "Text Files(*.txt)|*.txt|RichText Files(*.rtf)|*.rtf|CSharp Files(*.cs)|*.cs|All files(*.*)|*.*";
+            SaveFileDialog1.Filter = "Text Files(*.txt)|*.txt|RichText Files(*.rtf)|*.rtf|CSharp Files(*.cs)|*.cs|All files(*.*)|*.*";
+
+            // инициализируем список сборок
+            accemblyList = new List<string>();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        // Открытие файла
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string filename = OpenFileDialog1.FileName;
+
+            if (filename.EndsWith("txt")|| filename.EndsWith("cs"))
+            {
+                richTextBox1.LoadFile(filename, RichTextBoxStreamType.PlainText);
+            }
+            else if(filename.EndsWith("rtf"))
+            {
+                richTextBox1.LoadFile(filename, RichTextBoxStreamType.RichText);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SaveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string filename = SaveFileDialog1.FileName;
+
+            if (filename.EndsWith("txt") || filename.EndsWith("cs"))
+            {
+                richTextBox1.SaveFile(filename, RichTextBoxStreamType.PlainText);
+            }
+            else if (filename.EndsWith("rtf"))
+            {
+                richTextBox1.SaveFile(filename, RichTextBoxStreamType.RichText);
+            }
+        }
+
+        // Пункт меню Add Accembl - добавляем сборку
+        private void addAccemblyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string filename = OpenFileDialog1.FileName;
+
+            // Добавляем в список имя сборки
+            accemblyList.Add(filename.Remove(0, filename.LastIndexOf(@"\") + 1));
+        }
+
+
+        // Пункт меню Run Macros - запуск макроса
+        private void runMacrosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            else
+            {
+                string filename = OpenFileDialog1.FileName;
+                if (filename!="")
+                {
+                    Run_Macro(filename);
+                }
+            }
+        }
+
+
+        // Основная работа по запуске макроса
+        private void Run_Macro(string filename)
+        {
+            if (!filename.EndsWith("cs"))
+            {
+                MessageBox.Show("Макрос должен быть написан на c# и иметь расширение cs");
+                return;
+            }
+            try
+            {
+                // Читает как текст код из файла
+                string macroCode = new StreamReader(filename, Encoding.Default).ReadToEnd();
+
+                // Создает экземпляр компилятора c#
+                CSharpCodeProvider csProv = new CSharpCodeProvider();
+
+                // Создает набор параметров для передачи их компилятору.
+                CompilerParameters csParams = new CompilerParameters();
+
+                // Добавляет нужные сборки
+                foreach (string accembly in accemblyList)
+                {
+                    csParams.ReferencedAssemblies.Add(accembly);
+                }
+                accemblyList.Clear();
+
+                // Добавляет по умолчанию найболее используемые сборки
+                csParams.ReferencedAssemblies.Add("mscorlib.dll");
+                csParams.ReferencedAssemblies.Add("System.dll");
+                csParams.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+
+                // Не будем создавать из кода исполняемый файл
+                csParams.GenerateExecutable = false;
+                
+                // Код будет откомпилирован только в памяти без записи на диск
+                csParams.GenerateInMemory = true;
+                
+                // Компилируем код и получаем результаты компиляции
+                CompilerResults compResults = csProv.CompileAssemblyFromSource(csParams, macroCode);
+
+                // Проверяем на наличие ошибок
+                if (compResults.Errors.Count > 0)
+                {
+                    // Отображаем сообщение об ошибке
+                    foreach (var er in compResults.Errors)
+                    {
+                        MessageBox.Show(er.ToString());
+                    }
+                }
+                else
+                {
+                    // Создаем экземпляр временно созданного при компиляции класса
+                    object obj = compResults.CompiledAssembly.CreateInstance("Program");
+
+                    // В качестве аргумента программы возьмем наше текстовое поле, над которым можна производить манипуляции
+                    object[] args = new object[] {this.richTextBox1};
+                    
+                    // Вызываем код и передаем ему в качестве параметров наш обьект - класс Program и массив аргументов
+                    Type t = (Type) obj.GetType().InvokeMember("Main", BindingFlags.InvokeMethod, null, obj, args);
+                }
+            }
+            
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
