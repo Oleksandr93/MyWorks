fb2776da0727082dda14c33d35443b8ada1c32a4
Oleksandr93 <vivalasasha@gmail.com>
2016-02-07 11:55:45 +0200

    Ftp_Client

---
+++ FTP_client/FTP_client/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FTP_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.Location = new System.Drawing.Point(47, 81);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(377, 278);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView2_MouseDoubleClick);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Файл";
+            this.columnHeader3.Width = 177;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Дата";
+            this.columnHeader4.Width = 151;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(152, 414);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Подключиться";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(47, 382);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Text = "Логин";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(298, 382);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 3;
+            this.textBox5.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "FTP-сервер:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(148, 30);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(276, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 473);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ FTP_client/FTP_client/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using FTP_client; 
+
+
+namespace FTP_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FtpClient ftp = new FtpClient();
+
+        string serverDir = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+            menu.Items.Add("Сохранить файл");
+            menu.Items[0].Click += new EventHandler(Download_File);
+            menu.Items.Add("Загрузить файл на сервер");
+            menu.Items[1].Click += new EventHandler(Upload_File);
+            menu.Items.Add("Удалить каталог");
+            menu.Items[2].Click += new EventHandler(Remove_Directory);
+            menu.Items.Add("Удалить файл");
+            menu.Items[3].Click += new EventHandler(Delete_File);
+            listView2.ContextMenuStrip = menu;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text == null)
+            {
+                MessageBox.Show("Имя сервера не может быть пустым");
+                return;
+            }
+
+
+            ftp.Host = textBox6.Text;
+            ftp.UserName = textBox4.Text;
+            ftp.Password = textBox5.Text;
+            FileStruct[] FileList = DirectoryList("");
+            try
+            {
+                this.Text = "Подключение к ftp-серверу....";
+
+                foreach (FileStruct s in FileList)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = s.Name;
+                    lvi.SubItems.Add(s.CreateTime);
+                    listView2.Items.Add(lvi);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);              
+            }
+            this.Text = "FTP-client";
+        }
+
+        private FileStruct[] DirectoryList(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void listView2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView2.SelectedItems[0] == null)
+            {
+                MessageBox.Show("Выделите папку для просмотра");
+            }
+            try
+            {
+                this.Text = "Подключение к ftp-серверу....";
+                string directory = listView2.SelectedItems[0].SubItems[0].Text.Trim();
+
+                listView2.Items.Clear();
+
+                FileStruct[] FileList = ftp.ListDirectory("/" + directory);
+
+                this.Text = "Подключение к ftp-серверу....";
+
+                foreach (FileStruct s in FileList)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = s.Name;
+                    lvi.SubItems.Add(s.CreateTime);
+                    listView2.Items.Add(lvi);
+                }
+
+                textBox6.Text += ("/" + directory);
+                serverDir += ("/" + directory);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            this.Text = "FTP-client";
+        }
+
+        private void Delete_File(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems[0] == null)
+            {
+                MessageBox.Show("Выделите файл для удаления");
+            }
+            try
+            {
+                string directory = listView2.SelectedItems[0].SubItems[0].Text.Trim();
+                ftp.DeleteFile(serverDir + "/" + directory);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);               
+            }
+        }
+
+        private void Remove_Directory(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems[0] == null)
+            {
+                MessageBox.Show("Выделите файл для удаления");
+            }
+            try
+            {
+                string directory = listView2.SelectedItems[0].SubItems[0].Text.Trim();
+                ftp.RemoveDirectory(serverDir + "/" + directory);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);               
+            }
+        }
+
+        private void Download_File(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems[0] == null)
+            {
+                MessageBox.Show("Выделите файл для загрузки на локальный компьютер");
+            }
+            try
+            {
+                string directory = listView2.SelectedItems[0].SubItems[0].Text.Trim();
+                ftp.DownloadFile(serverDir, directory);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+
+        private void Upload_File(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            string file = openFileDialog1.FileName;
+            try
+            {
+                ftp.UploadFile(serverDir, file);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            {
+                
+            }
+        } 
+    }
+}
