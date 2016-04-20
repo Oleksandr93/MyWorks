e7986028392fab0ec35d1d24a10f6d85b42c8dc0
Oleksandr93 <vivalasasha@gmail.com>
2016-04-20 21:48:41 +0300

    MVC on PHP

---
+++ MVC on PHP/config/routes.php
@@ -0,0 +1,8 @@
+<?php
+    return array(
+       'news/([a-z]+)/([0-9]+)' => 'news/view/$1',          // actionView в NewsController
+        'news' => 'news/index',     // actionIndex в NewsController
+
+
+
+    );
